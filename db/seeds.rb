5d83627db56b44619c256e92d09d598937ef8cf4
Titus Mumo <titusmumo@gmail.com>
2023-07-12 12:10:36 +0300

    updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+# require 'active_record'
+
+# ActiveRecord::Base.establish_connection(
+#   adapter: 'postgresql',
+#   host: 'https://makawasco-backend.onrender.com/',
+#   database: 'backend_xxd8',
+#   username: 'backend_xxd8_user',
+#   password: 'LBzpIg1GcyH9MtmPPAZ0WySjIbwq2b23'
+# )
+
+
 
 Client.create(first_name: "John", last_name: "dave",)
