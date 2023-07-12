237c3a7d0140b91dd95ceb34ce6f88b3bf9523b9
Titus Mumo <titusmumo@gmail.com>
2023-07-12 22:13:58 +0300

    editing controllers

--- app/controllers/employee_clients_controller.rb
+++ app/controllers/employee_clients_controller.rb
@@ -1,43 +1,43 @@
-class EmployeeClientsController < ApplicationController
-  before_action :set_employee_client, only: %i[ show edit update destroy ]
+# class EmployeeClientsController < ApplicationController
+#   before_action :set_employee_client, only: %i[ show edit update destroy ]
 
-  # GET /employee_clients or /employee_clients.json
-  def index
-    @employee_clients = EmployeeClient.all
-  end
+#   # GET /employee_clients or /employee_clients.json
+#   def index
+#     @employee_clients = EmployeeClient.all
+#   end
 
-  # GET /employee_clients/1 or /employee_clients/1.json
-  def show
-  end
+#   # GET /employee_clients/1 or /employee_clients/1.json
+#   def show
+#   end
 
 
-  # GET /employee_clients/1/edit
-  def edit
-  end
+#   # GET /employee_clients/1/edit
+#   def edit
+#   end
 
-  # POST /employee_clients or /employee_clients.json
-  def create
+#   # POST /employee_clients or /employee_clients.json
+#   def create
    
-  end
+#   end
 
-  # PATCH/PUT /employee_clients/1 or /employee_clients/1.json
-  def update
+#   # PATCH/PUT /employee_clients/1 or /employee_clients/1.json
+#   def update
     
-  end
+#   end
 
-  # DELETE /employee_clients/1 or /employee_clients/1.json
-  def destroy
+#   # DELETE /employee_clients/1 or /employee_clients/1.json
+#   def destroy
 
-  end
+#   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_employee_client
-      @employee_client = EmployeeClient.find(params[:id])
-    end
+#   private
+#     # Use callbacks to share common setup or constraints between actions.
+#     def set_employee_client
+#       @employee_client = EmployeeClient.find(params[:id])
+#     end
 
-    # Only allow a list of trusted parameters through.
-    def employee_client_params
-      params.fetch(:employee_client, {})
-    end
-end
+#     # Only allow a list of trusted parameters through.
+#     def employee_client_params
+#       params.fetch(:employee_client, {})
+#     end
+# end
