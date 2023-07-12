39ac313cc68908dea705a5beadfa11743127b3f4
Titus Mumo <titusmumo@gmail.com>
2023-07-12 10:40:52 +0300

    creating migration

---
+++ db/migrate/20230712073754_create_clients.rb
@@ -0,0 +1,10 @@
+class CreateClients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :clients do |t|
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps
+    end
+  end
+end
