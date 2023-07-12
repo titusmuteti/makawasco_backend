456fe74365e89d20b4a23b12fe4b7567feab904d
Titus Mumo <titusmumo@gmail.com>
2023-07-12 10:48:49 +0300

    editing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :clients
-  resources :cars
+  # resources :cars
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
