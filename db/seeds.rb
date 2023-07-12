0b4e4c257e5ea5d2765142dbb47f93a35b27abac
Titus Mumo <titusmumo@gmail.com>
2023-07-12 11:01:30 +0300

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Client.create(first_name: "John", last_name: "dave",)
