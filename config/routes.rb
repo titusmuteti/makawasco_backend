477761fce37bedf67b6f0505717a804672e93a2a
Titus Mumo <titusmumo@gmail.com>
2023-07-14 09:53:49 +0300

    adding routes for login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :employees
   resources :clients
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  post "/signup", to: "clients#create"
+  get "/me", to: "clients#show"
 end
