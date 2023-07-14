7a485cedc9089b15eabec27191ebe2dcc37ca8a9
Titus Mumo <titusmumo@gmail.com>
2023-07-14 15:55:20 +0300

    editinf clients model

--- app/models/client.rb
+++ app/models/client.rb
@@ -7,6 +7,6 @@ class Client < ApplicationRecord
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :email, presence: true, uniqueness: true
-    validates :password, presence: true, length: { minimum: 6 }
-    validates :password, presence: true, confirmation: true
+    validates :password, presence: true, length: { minimum: 6 }, confirmation: true
+
 end
