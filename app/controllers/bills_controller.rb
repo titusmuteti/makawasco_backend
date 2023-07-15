83cdd4e4b8d524db8a150c45e647695d22e2773a
Titus Mumo <titusmumo@gmail.com>
2023-07-15 17:25:53 +0300

    edits

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,67 +1,32 @@
 class BillsController < ApplicationController
-  before_action :set_bill, only: %i[ show edit update destroy ]
-
-  # GET /bills or /bills.json
   def index
-    @bills = Bill.all
+      bills = Bill.all
+      render json: bills, status: :ok
   end
 
-  # GET /bills/1 or /bills/1.json
   def show
+      bill = Bill.find(params[:id])
+      render json: bill, status: :ok
   end
 
-  # GET /bills/new
-  def new
-    @bill = Bill.new
-  end
-
-  # GET /bills/1/edit
-  def edit
-  end
-
-  # POST /bills or /bills.json
   def create
-    @bill = Bill.new(bill_params)
-
-    respond_to do |format|
-      if @bill.save
-        format.html { redirect_to bill_url(@bill), notice: "Bill was successfully created." }
-        format.json { render :show, status: :created, location: @bill }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @bill.errors, status: :unprocessable_entity }
-      end
-    end
+      bill = Bill.create!(bill_params)
+      render json: bill, status: :created
   end
 
-  # PATCH/PUT /bills/1 or /bills/1.json
   def update
-    respond_to do |format|
-      if @bill.update(bill_params)
-        format.html { redirect_to bill_url(@bill), notice: "Bill was successfully updated." }
-        format.json { render :show, status: :ok, location: @bill }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @bill.errors, status: :unprocessable_entity }
-      end
-    end
+      bill = Bill.find(params[:id])
+      employee.update!(bill_params)
+      render json: bill, status: :ok
   end
 
-  # DELETE /bills/1 or /bills/1.json
   def destroy
-    @bill.destroy
-
-    respond_to do |format|
-      format.html { redirect_to bills_url, notice: "Bill was successfully destroyed." }
-      format.json { head :no_content }
-    end
+      bill = Bill.find(params[:id])
+      bill.destroy
+      render json: {message: "Bill deleted"}, status: :ok
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_bill
-      @bill = Bill.find(params[:id])
-    end
 
     # Only allow a list of trusted parameters through.
     def bill_params
--- app/serializers/bill_serializer.rb
+++ app/serializers/bill_serializer.rb
@@ -1,5 +1,5 @@
 class BillSerializer < ActiveModel::Serializer
-  attributes :id, :meter_no, :date_read, :previous_reading, :current_reading, :units_consumed, :balance, :date_paid, :client_id
+  attributes :id, :meter_no, :date_read, :previous_reading, :current_reading, :units_consumed, :balance, :date_paid
 
   belongs_to :client
 end
