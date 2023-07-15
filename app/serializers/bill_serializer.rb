755726980e4538ac36233263d3577c62090a2716
Titus Mumo <titusmumo@gmail.com>
2023-07-15 15:19:03 +0300

    adding relationships to bills

--- app/serializers/bill_serializer.rb
+++ app/serializers/bill_serializer.rb
@@ -1,3 +1,5 @@
 class BillSerializer < ActiveModel::Serializer
   attributes :id, :meter_no, :date_read, :previous_reading, :current_reading, :units_consumed, :balance, :date_paid, :client_id
+
+  belongs_to :client
 end
